3bc2f3ebe2a359d6cffcd3e1f71bf18ba375f637
Ara Juljulian <ara@juljulian.com>
2015-04-15 21:57:32 -0700

    Initial Commit

---
+++ BSCedarApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSCedarApp
+//
+//  Created by Ara Juljulian on 4/15/15.
+//  Copyright (c) 2015 Ara Juljulian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BSCedarApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BSCedarApp
+//
+//  Created by Ara Juljulian on 4/15/15.
+//  Copyright (c) 2015 Ara Juljulian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
